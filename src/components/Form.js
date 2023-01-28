8e357200f0325f0bc4dca63466a66fd2e6e59952
Charles Swaleh <charlswall@live.com>
2023-01-29 01:10:45 +0300

    Completed react forms code-along

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
-
+function Form({handleFirstNameChange, handleLastNameChange, firstName, lastName}) {
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
